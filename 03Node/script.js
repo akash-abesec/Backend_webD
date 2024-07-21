5b4063995d8af0e575e593b946a8981f2814321a
Akash Singh <112417241+akash-abesec@users.noreply.github.com>
2024-07-21 16:04:20 +0530

    Add files via upload

---
+++ 03Node/script.js
@@ -0,0 +1,24 @@
+// NPM understanding
+
+/* node js me jo installed hoke aata hai vo module kahlata hai */
+/* npm se dwnld hoke aata hai vo hota hai package */
+
+// installing and uninstalling anything basics & advanced
+// installing -> npm i one-liner-joke
+// uninstalling -> npm uninstall one-liner-joke
+// installing particular version -> npm i accessibility@3.0.9
+
+// understanding node_modules
+
+// dependencies -> packages and packages ki dependencies.
+// devdependencies -> aise pacakges jo sirf development me kaam aayenge par jab app ban
+// jayega aur depoy ho jayega to ham in packages ko use nii kar rahe honge .
+
+/*
+// scripts - understanding default scripts Path and custom scripts
+npm start
+npm run
+npm run concurrent
+*/
+
+console.log("Akash Singh");
