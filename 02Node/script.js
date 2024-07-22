133bae7564efc3f6259b90158f78eff3d28e01e5
Akash Singh <112417241+akash-abesec@users.noreply.github.com>
2024-07-22 22:17:15 +0530

    Update script.js

--- 02Node/script.js
+++ 02Node/script.js
@@ -1,4 +1,4 @@
-/* 
+
 1.Node.js Basics:-It is not a programming language, also not a technology, not framework, not a library.
                 It is a runtime environment for JavaScript. It is a platform built on Chrome's V8 JavaScript engine.
 2.Introduction to Node.js :- Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine. Node.js uses an event.
@@ -6,10 +6,10 @@
 4.Working with modules
 5.File system operations
 6.Understanding HTTP module
-*/
 
 
-/*---------------- writeFile -------------------//
+
+//---------------- writeFile -------------------//
 fs.writeFile(file, data[, options], callback)
 
 const fs = require('node:fs');
@@ -17,65 +17,62 @@ fs.writeFile("Hey.txt", "Hey hello kaise ho", function(err){
     if(err) console.error(err);
     else console.log("File created successfully");
 })
-*/
 
-/* ------------------- appendFile -----------------------
+// ------------------- appendFile -----------------------
 fs.appendFile(path, data[, options], callback)
 
-// const fs = require('node:fs');
-// fs.appendFile("Hey.txt", " Mai to achaa huu", function (err) {
-//     if (err) console.error(err);
-//     else console.log("File created successfully");
-// })
-*/
+const fs = require('node:fs');
+fs.appendFile("Hey.txt", " Mai to achaa huu", function (err) {
+    if (err) console.error(err);
+    else console.log("File created successfully");
+})
 
-/*
------------------------- rename ------------------
+
+
+//------------------------ rename ------------------
 fs.rename(oldPath, newPath, callback)
 const fs = require('node:fs');
 fs.rename("Hey.txt", "Hello.txt", function (err) {
     if (err) console.error(err);
     else console.log("File renamed successfully");
 })
-*/
 
-/*
-------------------- copyFile -----------------------
+
+
+//------------------- copyFile -----------------------
 fs.copyFile(src, dest, flags, callback)
 const fs = require('node:fs');
 fs.copyFile("Hello.txt", "./copy/copy.txt", function (err) {
     if (err) console.error(err);
     else console.log("File copied successfully");
 })
-*/
 
-/*
-------------------- unlink ------------------------
+
+//------------------- unlink ------------------------
 fs.unlink(path, callback)
 const fs = require('node:fs');
 fs.unlink("Hello.txt", function (err) {
     if (err) console.error(err);
     else console.log("Removed");
 })
-*/
 
-/*
------------------------- rmdir ------------------
+
+
+//------------------------ rmdir ------------------
 // It removes the directory if it is blank
 const fs = require('node:fs');
 fs.rmdir("./helloworld", { recursive: true }, function (err) {
     if (err) console.error(err);
     else console.log("removed");
 })
-*/
 
-/*
------------------------ mkdir --------------------
+
+//----------------------- mkdir --------------------
 fs.mkdir(path[, options], callback)
 const fs = require('node:fs');
 fs.mkdir("./helloworld", function (err) {
     if (err) console.error(err);
     else console.log("created succesfully");
 })
-*/
+
 
